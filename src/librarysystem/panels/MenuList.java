9a1b33f1cc1ac74eff6312c9bdc1263e2bd92529
bikram455 <bikramleeemboo@gmail.com>
2022-10-13 14:12:30 -0500

    * merge conflict 1.

--- src/librarysystem/panels/MenuList.java
+++ src/librarysystem/panels/MenuList.java
@@ -1,8 +1,12 @@
 package librarysystem.panels;
 
-import javax.swing.JPanel;
-import javax.swing.JList;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
 
 import librarysystem.SecondForm;
@@ -10,15 +14,23 @@ import librarysystem.SecondForm;
 public class MenuList extends JPanel {
 	public JList menuList;
 	public SecondForm lib;
+	public HashMap<String, String> options;
 	/**
 	 * Create the panel.
 	 */
-	public MenuList(SecondForm lib) {
+	public MenuList(SecondForm lib, String access) {
 		this.lib = lib;
 		setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(170, 121, 65), new Color(170, 121, 65), new Color(170, 121, 65), new Color(170, 121, 65)));
 		setBackground(new Color(254, 255, 255));
-
-		String[] items = getMenuList();
+		
+		options = new HashMap<>();
+		options.put("Checkout Book", "LIBRARIAN");
+		options.put("Search Member", "LIBRARIAN");
+		options.put("Add Member", "ADMIN");
+		options.put("Add Book", "ADMIN");
+//		options.put("ADMIN", "Add Book");
+//		options.put("ADMIN", "Add Book");
+		String[] items = getMenuList(access);
 
 		menuList = new JList(items);
 //		menuList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -37,8 +49,19 @@ public class MenuList extends JPanel {
 
 	}
 
-	public String[] getMenuList() {
-		String[] items = {"Logout", "Add Book", "Add Member", "Search Book"};
+	public String[] getMenuList(String access) {
+		List<String> itemList = new ArrayList<>();
+		if(access.equals("BOTH")) {
+			for(String k: options.keySet()) itemList.add(k);
+		} else {
+			for(String k: options.keySet()) {
+				if(access.equals(options.get(k))) {
+					itemList.add(k);
+				}
+			}	
+		}
+		itemList.add("Logout");
+		String[] items = itemList.toArray(String[]:: new);
 		return items;
 	}
 }
