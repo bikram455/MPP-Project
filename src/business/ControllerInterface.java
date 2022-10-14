af18283a2b1112220c129841831879ced37d30f4
bikram455 <bikramleeemboo@gmail.com>
2022-10-14 10:43:04 -0500

    * moved login system to controller.

--- src/business/ControllerInterface.java
+++ src/business/ControllerInterface.java
@@ -2,12 +2,10 @@ package business;
 
 import java.util.List;
 
-import business.Book;
-import dataaccess.DataAccess;
-import dataaccess.DataAccessFacade;
+import dataaccess.User;
 
 public interface ControllerInterface {
-	public void login(String id, String password) throws LoginException;
+	public User login(String id, String password) throws LoginException;
 	public List<String> allMemberIds();
 	public List<String> allBookIds();
 	
--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -1,30 +1,30 @@
 package business;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import dataaccess.Auth;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessFacade;
+import dataaccess.TestData;
 import dataaccess.User;
 
 public class SystemController implements ControllerInterface {
 	public static Auth currentAuth = null;
 	
-	public void login(String id, String password) throws LoginException {
-		DataAccess da = new DataAccessFacade();
-		HashMap<String, User> map = da.readUserMap();
-		if(!map.containsKey(id)) {
-			throw new LoginException("ID " + id + " not found");
-		}
-		String passwordFound = map.get(id).getPassword();
-		if(!passwordFound.equals(password)) {
-			throw new LoginException("Password incorrect");
-		}
-		currentAuth = map.get(id).getAuthorization();
-		
-	}
+//	public void login(String id, String password) throws LoginException {
+//		DataAccess da = new DataAccessFacade();
+//		HashMap<String, User> map = da.readUserMap();
+//		if(!map.containsKey(id)) {
+//			throw new LoginException("ID " + id + " not found");
+//		}
+//		String passwordFound = map.get(id).getPassword();
+//		if(!passwordFound.equals(password)) {
+//			throw new LoginException("Password incorrect");
+//		}
+//		currentAuth = map.get(id).getAuthorization();
+//		
+//	}
 	@Override
 	public List<String> allMemberIds() {
 		DataAccess da = new DataAccessFacade();
@@ -40,6 +40,24 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readBooksMap().keySet());
 		return retval;
 	}
-	
+
+	@Override
+	public User login(String userName, String password) throws LoginException {
+		TestData test = new TestData();
+		List<User> users = test.allUsers;
+
+		
+		
+		User u = null;
+		for (User user : users) {
+			if (userName.contains(user.getId()) && password.contains(user.getPassword())) {
+				u = user;
+				
+				break;
+			}
+		}
+		if(u == null) throw new LoginException("Username or password incorrect!");
+		return u;
+	}
 	
 }
